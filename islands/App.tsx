857182dd6cd82bba127b1cabb7fbdb7c9245c16f
manybugsdev <manybugsdev@users.noreply.github.com>
2023-06-15 21:47:11 +0900

    style: ux

--- islands/App.tsx
+++ islands/App.tsx
@@ -52,34 +52,32 @@ export default (props: { count: number }) => {
       bc.close();
     };
   }, [bc]);
+  function onClick() {
+    fetch("/api/count", {
+      method: "POST",
+      body: JSON.stringify(count + 1),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((c) => {
+          setCount((n) => c > n ? c : n);
+        });
+      }
+    });
+    setCount((n) => n + 1);
+    bc.postMessage("countup");
+    confettiRandom();
+  }
   return (
     <div class="flex w-full h-screen-d bg-gray-800 text-green-300 relative overflow-hidden">
       {new Array(Math.ceil(count / 100)).fill(0).map((_) => <Denosaur />)}
       {count >= 10000 && <Rain />}
       <div
-        class="m-auto flex flex-col justify-center items-center z-20 bg-black rounded-xl bg-opacity-20 w-96 h-96"
-        style={{ "backdrop-filter": "blur(1px)" }}
+        class="m-auto flex flex-col justify-center items-center z-20 bg-black rounded-xl bg-opacity-80 w-96 h-96 select-none cursor-pointer"
+        onClick={onClick}
       >
         <p class="text-6xl font-bold">{count}</p>
         <p>/ 10000</p>
-        <button
-          class="border p-4 rounded mt-4 select-none"
-          onClick={() => {
-            fetch("/api/count", {
-              method: "POST",
-              body: JSON.stringify(count + 1),
-            }).then((res) => {
-              if (res.ok) {
-                res.json().then((c) => {
-                  setCount((n) => c > n ? c : n);
-                });
-              }
-            });
-            setCount((n) => n + 1);
-            bc.postMessage("countup");
-            confettiRandom();
-          }}
-        >
+        <button class="border p-4 rounded mt-4 select-none">
           Count Up
         </button>
       </div>
